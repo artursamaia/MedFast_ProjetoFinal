078dfa26bf476ae0a147288eed01b214afbce1ef
ARTUR MAIA <arturzanett0072015@gmail.com>
2025-06-15 21:56:25 -0300

    Implementando Mascara de Cpf e Tel no Formulario

--- js/script.js
+++ js/script.js
@@ -6,10 +6,26 @@ const content = document.querySelector('.content');
 
 menuToggle.addEventListener('click', () => {
     if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-        content.style.marginLeft = '0';
+        sidebar.style.width = '55px';
+        content.style.marginLeft = '0px';
     } else {
         sidebar.style.width = '250px';
         content.style.marginLeft = '250px';
     }
 });
+
+if (document.body.id === 'page-form') {
+    //Mascara para Telefone especifico para a pagina de Formulario
+    const telefoneInput = document.getElementById('telefone');
+    const phoneMaskOptions = {
+        mask: '(00) 00000-0000'
+    };
+    const phoneMask = IMask(telefoneInput, phoneMaskOptions);
+
+    //Mascara Para CFP especifico para a pagina de Formulario
+    const cpfInput = document.getElementById('cpf');
+    const cpfMaskOptions = {
+        mask: '000.000.000-00'
+    }
+    const cpfMask = IMask(cpfInput, cpfMaskOptions);
+}
